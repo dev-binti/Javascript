624c6e48d54df3e02153fc71384eb084cdb85409
Tajunnahar Boby <tajunnaharbinti@gmail.com>
2026-09-20 11:25:58 +0600

    javascript 5 section done

---
+++ js/script.js
@@ -0,0 +1,45 @@
+console.log("hello world")
+
+
+let a = 10
+let b = 20
+console.log(a+b)
+
+let x
+console.log(x = 6)
+
+let firstname = "john"
+let lastname = "doe"
+console.log("john"+""+"doe")
+
+let A,B,c;
+A = 5;
+B = 6;
+c = A+B;
+
+let X = 5;                       // Declare X , given it the value of 5
+let y = X + 2                    // Declare y , given it the value of X+2
+
+let $ = "hello world"
+console.log($)
+
+let _firstname = "tajunnahar"
+console.log(_firstname)
+
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+
+let t = 10
+let u = 5
+t **= u
+console.log(t)
+
+let v =45
+v:45
+console.log(v)
+
+let I = 4
+let r = 5
+I &&= r
+console.log(I)
